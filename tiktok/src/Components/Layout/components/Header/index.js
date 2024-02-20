824c70eb088ba1153dd174a6e53edcda139d63f4
Nguyễn Trung Hiếu <nth.doremon@gmail.com>
2024-02-20 14:03:07 +0700

    Bị khang dí dealine dừng

--- tiktok/src/Components/Layout/components/Header/index.js
+++ tiktok/src/Components/Layout/components/Header/index.js
@@ -1,5 +1,9 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -8,7 +12,25 @@ function Header() {
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 {/* Logo  */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
                 {/* Search */}
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        {/* Icon Clear  */}
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    {/* Loading  */}
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        {/* icon sreach */}
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
             </div>
         </header>
     );
